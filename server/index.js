7a574b4873dbccb2a67eb2cd938019971c87d1fd
Samreen <samreensheik706@gmail.com>
2026-05-06 13:28:35 +0530

    admindashboard

--- server/index.js
+++ server/index.js
@@ -288,39 +288,50 @@ app.get("/myOrders/:userId", async (req, res) => {
   }
 });
 
-app.get("/admin-stats",async(req,res)=>{
-  try{
-    const products= await ProductsModel.countDocuments();
-    const users=await UsersModel.countDocuments();
-    const orders=await OrdersModel.find();
+app.get("/admin-stats", async (req, res) => {
+  try {
+    const products = await ProductsModel.countDocuments();
+    const users = await UsersModel.countDocuments();
+    const orders = await OrdersModel.find();
 
-    const totalOrders=orders.length;
+    const totalOrders = orders.length;
 
-    let delivered=0;
-    let pending=0;
+    let delivered = 0;
+    let pending = 0;
 
-    const ordersByDate={};
-    orders.forEach(order=>{
-      const date=new Date(order.createdAt).toLocalDateString();
-      if(order.status==="Delivered") 
+    const ordersByDate = {};
+
+    orders.forEach(order => {
+
+      const date = new Date(order.createdAt).toLocaleDateString();
+
+      if (order.status === "Delivered")
         delivered++;
-      else pending++;
+      else
+        pending++;
 
-      if(ordersByDate[date])
-      {
+      if (ordersByDate[date]) {
         ordersByDate[date]++;
-      } else{
-        ordersByDate[date]=1;
+      } else {
+        ordersByDate[date] = 1;
       }
     });
 
-    const chartData =Object.keys(ordersByDate).map(date=>({
+    const chartData = Object.keys(ordersByDate).map(date => ({
       date,
-      orders:ordersByDate[date]
+      orders: ordersByDate[date]
     }));
 
-    res.json({products,users,orders,totalOrders,delivered,pending,chartData});
-  }catch(err){
+    res.json({
+      products,
+      users,
+      totalOrders,
+      delivered,
+      pending,
+      chartData
+    });
+
+  } catch (err) {
     console.log(err);
     res.status(500).json("Error");
   }
